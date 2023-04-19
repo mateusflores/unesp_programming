d14e9aa89f1a6438cd407e942e485869b8507aa1
mateusflores <mateusgf.42@gmail.com>
2023-04-18 21:55:48 -0300

    Add funções addAlunos, imprimeLista e está limitando a quantidade de alunos a ser adicionada

--- ED1/ex1.c
+++ ED1/ex1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct {
+typedef struct aluno {
     int matricula;
     char nome[30];
     double np1, np2, notaEx;
-    Aluno *prox;
+    struct aluno *prox;
 } Aluno;
 
 int quantidadeAlunos(Aluno *head) {
@@ -15,12 +16,57 @@ int quantidadeAlunos(Aluno *head) {
         num++;
         aux = aux->prox;
     }
-
     return num;
 }
 
+Aluno* addAlunos(int quantidadeAlunos) {
+    // Primeiro registro
+    Aluno *aux = malloc(sizeof(Aluno));
+    Aluno *head = aux;
+    
+    for (int i=0; i<quantidadeAlunos; i++) {
+        aux->matricula = i+1;
+        getc(stdin);
+        puts("Nome: ");
+        fgets(aux->nome, 30, stdin);
+        puts("Nota da prova 1: ");
+        scanf("%lf", &aux->np1);
+        puts("Nota da prova 2: ");
+        scanf("%lf", &aux->np2);
+        puts("Nota do exercicio: ");
+        scanf("%lf", &aux->notaEx);
+
+        // Criando próximo node ou atribuindo NULL caso esteja no final da lista
+        if (i != quantidadeAlunos-1) {
+            aux->prox = malloc(sizeof(Aluno));
+            aux = aux->prox;
+        } else 
+            aux->prox = NULL;
+    }
+    return head;
+}
+
+void imprimeLista (Aluno *head) {
+    Aluno *aux;
+    puts("\nNome  Matricula   N1  N2  NotaEx");
+    for (aux=head; aux != NULL; aux = aux->prox) {
+        printf("%s  %d  %lf  %lf  %lf\n", aux->nome, aux->matricula, aux->np1, aux->np2, aux->notaEx);
+    }
+}
+
+
+
 int main() {
-    Aluno *head;
+    int quant;
+    puts("Quantos alunos deseja inserir? (max: 50 alunos)");
+    do {
+        scanf("%d", &quant);
+        if (quant > 50)
+            printf("Numero invalido. O numero maximo de alunos deve ser menor que 50.\nInsira novamente:\n");
+    } while (quant > 50);
 
+    Aluno *lista = addAlunos(quant);
+    printf("Quantidade de alunos inseridos: %d", quantidadeAlunos(lista));
 
+    imprimeLista(lista);
 }
