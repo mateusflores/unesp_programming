b641f83385dbdc428e9bf6061582d1e91b76f8e8
mateusflores <mateusgf.42@gmail.com>
2023-04-24 19:55:08 -0300

    criação do documento e definição da struct

---
+++ ED1/ex2/brinde_face.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct sequencia {
+    char a, b, c, d;
+    struct sequencia *ant;
+    struct sequencia *prox;
+} Sequencia;
+
+int main() {
+
+}
