41797a6cd8c2bb83f3e53785a8bfe2fb419bcc74
MateusFlores <mateusgf.42@gmail.com>
2023-04-28 16:27:17 -0300

    Exercício face.c está funcionando com a maioria dos casos, mas ainda ocorrem erros de Segmentation fault

--- ED1/ex2_stack/face.c
+++ ED1/ex2_stack/face.c
@@ -12,12 +12,33 @@ typedef struct {
     Node *topo;
 } Stack;
 
-void inicializaPilha(Stack *pilha) {
-    pilha->topo = NULL;
+// Se a pilha estiver vazia, atribui "FACE" ao elemento no topo 
+bool checkTopoVazio(Stack *pilha) {
+    if (pilha->topo == NULL) {
+        pilha->topo = malloc(sizeof(Node));
+
+        pilha->topo->a = 'F';
+        pilha->topo->b = 'A';
+        pilha->topo->c = 'C';
+        pilha->topo->d = 'E';
+        pilha->topo->prox = NULL;
+
+        return true;
+    } else
+        return false;
 }
 
-bool pilhaVazia(Stack *pilha) {
-    return (pilha->topo == NULL);
+void inicializaPilha(Stack *pilha) {
+    pilha->topo = NULL;
+    checkTopoVazio(pilha);
+}
+
+// Verifica se o item no topo eh igual ao item anterior na pilha
+bool checkReverso(Node *atual, Node *anterior) {
+    if (atual->a == anterior->d && atual->b == anterior->c && atual->c == anterior->b && atual->d == anterior->a)
+        return true;
+    else
+        return false;
 }
 
 void insereItem(Stack *pilha, char frase[]) {
@@ -33,21 +54,56 @@ void insereItem(Stack *pilha, char frase[]) {
     pilha->topo = aux;
 }
 
+// Remove o item do topo e o próximo, caso eles sejam reversos
 void removeItens(Stack *pilha) {
+    for (int i=0; i<2; i++) {
+        Node *aux = pilha->topo;
+        pilha->topo = pilha->topo->prox;
+        free(aux);
+    }
+}
+
+int premiaParticipante(Stack *pilha) {
+
+    if (checkReverso(pilha->topo, pilha->topo->prox)) {
+        removeItens(pilha);
+        return 1;
+    }
     
 }
 
+int entradasPainel(Stack *pilha, int num) {
+    int quantidadeGanhadores = 0;
+    char frase[5];
+    for (int i=0; i<num; i++) {
+        fscanf(stdin, "%s", frase);
+        insereItem(pilha, frase);
+        quantidadeGanhadores += premiaParticipante(pilha);
+    }
+    return quantidadeGanhadores;
+}
+
+
+
+
 int main() {
-    Stack *pilha;
-    inicializaPilha(pilha);
+    int quantidadeGanhadores = 0;
+    Stack pilha;
+    inicializaPilha(&pilha);
 
-    int quantidade;
-    scanf("%d", &quantidade);
+    int num;
+    scanf("%d", &num);
 
-    char resposta[5];
-    getc(stdin);
-    fgets(resposta, 5, stdin);
-    insereItem(pilha, resposta);
+    quantidadeGanhadores = entradasPainel(&pilha, num);
+
+
+
+    // TESTANDO ENTRADAS NA PILHA
+    for (Node *aux = pilha.topo; aux != NULL; aux = aux->prox) {
+        printf("\n%c %c %c %c", aux->a, aux->b, aux->c, aux->d);
+    }
+
+    printf("\nQuantidade de ganhadores: %d\n", quantidadeGanhadores);
 
 
 }
