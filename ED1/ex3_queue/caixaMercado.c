91c4afe9f9ba07957bcda991ac8e0675689c8524
MateusFlores <mateusgf.42@gmail.com>
2023-05-19 14:05:11 -0300

    Exercício 3 de ED1 adicionado, junto das principais funções de lista duplamente encadeada

--- ED1/ex3_queue/caixaMercado.c
+++ ED1/ex3_queue/caixaMercado.c
@@ -2,9 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "fila.h"
-#include "filaPrioridade.h"
+
+typedef struct {
+    char nome[30];
+    bool ehPrioritario;
+    int itensCarrinho;    
+} Cliente;
+
+typedef struct node {
+    struct node *ant;
+    struct node *prox;
+    Cliente cliente;
+} Node;
+
+typedef struct {
+    Node *inicio;
+    Node *fim;
+} Fila;
+
+bool verificaVazia (Fila *fila) {
+    if (fila->inicio == NULL)
+        return true;
+    else
+        return false;
+}
+
+void inicializaFila (Fila *fila) {
+    fila->fim = NULL;
+    fila->inicio = NULL;
+}
+
+void insereCliente (Fila *fila, Cliente cliente) {
+    Node *aux = (Node*) malloc(sizeof(Node));
+    aux->cliente = cliente;
+    aux->prox = NULL;
+
+    if (fila->inicio == fila->fim) {
+        fila->inicio = aux;
+        fila->fim = aux;    
+    } else {
+        fila->fim->prox = aux;
+        fila->fim = aux;
+    }
+}
+
+void removeCliente (Fila *fila, Node *cliente) {
+    if (verificaVazia(fila)) {
+        return;
+    } else {
+        Node *aux = fila->inicio;
+        if (fila->inicio == fila->fim) {
+            inicializaFila(fila);
+        } else {
+            fila->inicio = fila->inicio->prox;
+            fila->inicio->ant = NULL;
+        }
+        free(aux);
+    }
+}
+
+int tamanhoFila (Fila *fila) {
+    int tam = 0;
+    Node *aux;
+    for (aux=fila->inicio; aux=fila->inicio->prox; aux->prox != NULL) {
+        tam += 1;
+    }
+    return tam;
+}
+
+Cliente criaCliente(char nome[30], bool ehPrioritario, int itensCarrinho) {
+    Cliente c;
+    strcpy(c.nome, nome);
+    c.ehPrioritario = ehPrioritario;
+    c.itensCarrinho = itensCarrinho;
+
+    return c;
+}
 
 int main() {
-    
+    Fila *fila = malloc(sizeof(Fila));
+    inicializaFila(fila);
+
+    Cliente c1 = {"Mateus", false, 14};
+    Cliente c2 = {"Petrúcio", false, 30};
+    Cliente c3 = {"Leona", true, 5};
+    Cliente c4 = {"Haru", false, 11};
+
+    insereCliente(fila, c1);
+    insereCliente(fila, c2);
+    insereCliente(fila, c3);
+
+    if (verificaVazia(fila))
+        printf("\nVazia\n\n");
+    else 
+        printf("\nNao Vazia\nTamanho: %d\n\n", tamanhoFila(fila));
 }
--- ED1/ex3_queue/fila.h
+++ ED1/ex3_queue/fila.h
@@ -1,35 +1,3 @@
-#include "cliente.h"
+#include <stdlib.h>
 
-typedef struct node {
-    struct node *ant;
-    struct node *prox;
-    Cliente cliente;
-} Node;
-
-typedef struct {
-    Node *inicio;
-    Node *fim;
-} Fila;
-
-Fila* inicializaFila () {
-    Fila *fila = (Fila*) malloc(sizeof(Fila));
-    if (fila != NULL)
-        *fila = NULL;
-    return fila;
-}
-
-void insereCliente (Fila *fila, Node *cliente) {
-
-}
-
-void removeCliente (Fila *fila, Node *cliente) {
-    
-}
-
-bool verificaVazia (Fila *fila) {
-
-}
-
-int tamanhoFila (Fila *fila) {
-
-}
+int *h = malloc(sizeof(int));
