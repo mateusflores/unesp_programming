26367c19e6f6701c89a6cf2d3fb5af9e95353eff
MateusFlores <mateusgf.42@gmail.com>
2023-05-23 16:23:34 -0300

    add representação de uma matriz esparsa

--- ED1/lista01/ex06.c
+++ ED1/lista01/ex06.c
@@ -7,6 +7,22 @@ typedef struct intGrande {
     int potencia;
 } IntGrande;
 
+
+// Representação de matriz esparsa (elemento)
+typedef struct elementoMatrizEsparsa {
+    int linha;
+    int coluna;
+    int valor;
+    struct elementoMatrizEsparsa *prox;
+} ElementoMatrizEsparsa;
+
+// Lista da matriz esparsa, que contém apenas os elementos diferentes de 0 e o tamanho da matriz
+typedef struct {
+    ElementoMatrizEsparsa *primeiro;
+    int numLinhas;
+    int numColunas;
+} MatrizEsparsa;
+
 typedef struct pilha {
 
 } Pilha;
