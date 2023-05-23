16a9c1a1adae0008f2b345d7f7fb83c678e13ff6
MateusFlores <mateusgf.42@gmail.com>
2023-05-23 14:01:35 -0300

    Estrutura do exercício 6 para formar inteiros grandes

---
+++ ED1/lista01/ex06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct intGrande {
+    struct intGrande *prox;
+    int unidade;
+    int potencia;
+} IntGrande;
+
+typedef struct pilha {
+
+} Pilha;
+
+int main() {
+
+}
