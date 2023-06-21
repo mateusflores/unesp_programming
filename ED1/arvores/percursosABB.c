9cbadeeb6a05f80601b477e0abb1dd44ae8a3409
MateusFlores <mateusgf.42@gmail.com>
2023-06-21 17:55:05 -0300

    adicionadas as funções para inserção de dados na árvore, deletar arvore e representação em pré-ordem, pós-ordem e inordem.

--- ED1/arvores/percursosABB.c
+++ ED1/arvores/percursosABB.c
@@ -1,38 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct node *p_node;
 
 struct node {
-    int valor;
-    p_node pai, direita, esquerda;
+    int chave;
+    p_node direita, esquerda;
 };
 
-p_node criarArvore(int chave, p_node esq, p_node dir) {
+p_node criarArvore(int chave) {
     p_node raiz = malloc(sizeof(struct node));
-    raiz->valor = chave;
-    raiz->direita = dir;
-    raiz->esquerda = esq;
+    raiz->chave = chave;
+    raiz->direita = NULL;
+    raiz->esquerda = NULL;
     return raiz;
 }
 
-p_node procurarPosicao(int valor, p_node raiz) {
-    if (raiz == NULL || raiz->valor == valor) {
-        criarArvore(valor, NULL, NULL);
+p_node inserirChave(p_node raiz, int valor) {
+    if (raiz->direita == NULL && raiz->chave < valor)
+        return raiz->direita = criarArvore(valor);
+    if (raiz->esquerda == NULL && raiz->chave > valor)
+        return raiz->esquerda = criarArvore(valor);
+    if (raiz->chave == valor)
         return raiz;
-    }
     
-    if (valor < raiz->direita->valor){ 
-        return procurarPosicao(valor, raiz->esquerda);
-    } else { 
-        return procurarPosicao(valor, raiz->direita);
+    if (raiz->chave < valor)
+        return inserirChave(raiz->direita, valor);
+    else 
+        return inserirChave(raiz->esquerda, valor);
+    
+}
+
+void pre_ordem(p_node raiz) {
+    if (raiz != NULL) {
+        printf("%d ", raiz->chave);
+        pre_ordem(raiz->esquerda);
+        pre_ordem(raiz->direita);
     }
 }
 
-int main() {
-    int valores[5] = [10, 3, 12, 5, 11];
+void pos_ordem(p_node raiz) {
+    if (raiz != NULL) {
+        pos_ordem(raiz->esquerda);
+        pos_ordem(raiz->direita);
+        printf("%d ", raiz->chave);
+    }
+}
 
-    for (int i = 0; i < 5; i++)
+void inordem(p_node raiz) {
+    if (raiz != NULL) {
+        inordem(raiz->esquerda);
+        printf("%d ", raiz->chave);
+        inordem(raiz->direita);
+    }
+}
+
+void deletarArvore(p_node raiz) {
+    if (raiz != NULL) {
+        deletarArvore(raiz->esquerda);
+        deletarArvore(raiz->direita);
+        free(raiz);
+        printf("0 ");
+    }
+}
+
+void entradaDados() {
+
+}
+
+int main() {
+    int valores[5] = {8, 3, 12, 5, 11};
+
+    p_node raiz = criarArvore(10);
+    
+    for (int i = 0; i < 5; i++) {
+        inserirChave(raiz, valores[i]);
+    }
+
+    printf("\nPre: ");
+    pre_ordem(raiz);
+    printf("\nPos: ");
+    pos_ordem(raiz);
+    printf("\nIn: ");
+    inordem(raiz);
+    printf("\n");
+
+    deletarArvore(raiz);
         
 
 }
